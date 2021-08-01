f1c6ea4a3248af141056d9835219cf54977eb1ca
Abijah <abijah.nez@gmail.com>
2021-08-01 02:16:08 -0400

    Added the API

--- herovillainsearch/search/views.py
+++ herovillainsearch/search/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        character = request.POST['data']
+        data = {}
+        res = requests.get('https://superheroapi.com/api.php/232593295392235/search/' + character).json()
+
+       
+
+        
+
+        
+
+        return render(request, 'index.html', {'res': res} )
+    else:
+        return render(request, 'index.html')
