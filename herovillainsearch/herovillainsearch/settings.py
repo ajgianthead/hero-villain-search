b1ba36e7b8a03a43156d6d10dc7af4386d8c44da
Abijah <abijah.nez@gmail.com>
2021-08-08 10:06:27 -0400

    moved some stuff

--- herovillainsearch/herovillainsearch/settings.py
+++ herovillainsearch/herovillainsearch/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.json(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
+
+
+
